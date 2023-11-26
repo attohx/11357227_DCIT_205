f35c2140a9b3b9f930c92b5d9ac6c021ff8db90d
Nathan Attoh <122324996+attohx@users.noreply.github.com>
2023-11-26 10:53:37 +0000

    Updated Page

--- int_students.php
+++ int_students.php
@@ -16,25 +16,38 @@
     @include "include/navbar.php";
 ?> 
 
-<div class="main-container">
+<div class= "main-container">
 
-<section class="sect-gap">
-    <div class="container">
-        <div class="section-align">
-            <div class="section-align-content">
-                <div class="title">
-                    <h4>International Students Information</h4>
-                    <p>This is the Information Technology age and at its core is Computer Science. The study of computer science is therefore critical if society is to take full advantage of the benefits Information Technology has to offer and make rapid economic advances. Computer science involves the study and development of applications that allow for our everyday use of computers, the software systems that support these applications, and the means and methods used to produce both applications and systems that behave reliably, correctly and ethically. At the heart of Computer Science lies the skill of computer programming and employers who hire Computer Scientists expect adept programmers with knowledge of current techniques and tools and the ability to apply sound engineering principles to the development, construction and maintenance of computer programs.
-
-The range of courses available in the department and the skills acquired by students enable them to find employment in many diverse fields of the economy and prepare our graduates for the computing industry, working in a project team (or research and development team), and to enable the graduate to bring specialist skills to that team. The course also provides the platform for further study or research through exposure to established and emerging technologies and methods, and to active fields of research in Computer Science.
-
-The Department offers two undergraduate degree programmes, Bachelor of Science in Computer Science Single-Major (3:2:1:1) and Major-Minor (3:2:2:1) as well as Bachelor of Science in Information Technology.</p>
-                </div>
+    <section class="sect-gap-int">
+        <div class="container">
+            <div class="section-align">
+                <div class="section-align-content">
+                    <div class="card text-center" >
+                        <div class="card-body" >
+                            <h5 class="card-title">International Students</h5>
+                            <div class="text-center">
+                                <img src="media/img/intl_main_2.jpg" class="img-fluid img-thumbnail" width="50%"></img>
+                            </div>
+                            <p>The University of Ghana announces for the information of the general public that applications are open for the admission of prospective applicants into various undergraduate programmes for the 2022/2023 academic year. Applicants should take note of the following</p>
+                            <ol class="list-styled">
+                                <li>Undergraduate applications are accepted all year round. However the academic year begins in September.</li>
+                                <li>All applicants who will be writing WASSCE in May/June 2023 are eligible to apply.</li>
+                                <li>There is direct admission of applicants into all programmes offered by the College of Health Sciences but as first choice only. For second and third choices, applicants are strongly advised to choose courses from the College of Basic and Applied Sciences.</li>
+                                <li>There is direct admission of applicants into the various Programmes available in the School of Physical and Mathematical Sciences and the School of Biological Sciences. Applicants will be expected to select up to four (4) choices in order of preference.</li>
+                                <li>Applicants who select either B.Sc. Administration or Bachelor of Laws (LLB) as first choice are advised strongly to select bouquets from the Bachelor of Arts programme as second choice to enhance their chances of gaining admission.</li>
+                                <li>For the Bachelor of Arts (BA) programme, applicants are required to select subjects from the list of bouquets provided.  Up to four (4) subject bouquets must be selected in order of preference.</li>
+                                <li>All students entering the BA progamme must offer three (3) subjects at Level 100, two (2) subjects at Level 200 and Level 300 and either two (2) subjects or one (1) subject at Level 400.</li>
+                                <li>Holders of other qualifications including International Baccalaureate (IB), IGCSE, and the American Grade 12 examinations which hitherto were admitted to Level 200, will be admitted to Level 100.</li>
+                                <li>Since the 2007/2008 academic year, the University of Ghana has ceased to admit applicants with the West African Examination Council’s Ordinary and Advanced Level Certificates</li>
+                                <li>All Applicants must take note that the process for admission to the University of Ghana is very transparent.  Applicants should therefore beware of any persons who may approach them on the pretext of assisting them gain admission for a fee.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>     
             </div>
-        </div>
 
-    </div>
-</section>
+        </div>
+    </section>
 
 </div>
 
