d52bbb1d66a7899c6285d96ca39f49e363066e95
Nathan Attoh <122324996+attohx@users.noreply.github.com>
2023-11-28 21:30:12 +0000

    small edits

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,8 +1,28 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+let slideIndex = 1;
+let imageContainer = document.querySelector(".image-container");
+let buttons = document.querySelectorAll(".buttons button");
 
-buttons.array.forEach(button => {
-    button.addEventListener("click",() => {
+function showSlide(n) {
+    slideIndex = n;
+    imageContainer.style.left = -500 * (n - 1) + "px";
+    buttons.forEach(button => button.classList.remove("active"));
+    buttons[n - 1].classList.add("active");
+}
 
-    })
-    
-});
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex > 5) {
+    slideIndex = 1;
+    }
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    if (slideIndex < 1) {
+    slideIndex = 5;
+    }
+    showSlide(slideIndex);
+}
+
+setInterval(nextSlide, 3000); // Change the slide every 3 seconds
